4dbcc6e14577a276a019953ecedd5db2b6aaa553
GrigoriGit <grigorikurilenko@gmail.com>
2025-12-12 15:50:07 +0300

    Практическая 6 задание 2

---
+++ PZ_6_2.py
@@ -0,0 +1,23 @@
+#Дано число R и список A размера N. Найти элемент списка, который наиболее
+#близок к числу R (то есть такой элемент AK, для которого величина |AK - R| является
+#минимальной).
+import random
+try:
+    N = int(input("Введите размеры списка - "))
+    R = int(input("Введите число R - "))
+except ValueError:
+    print("Ошибка, вы ввели не число")
+A = []
+smallest = None
+for i in range(N+1):
+    A.append(random.randint(0,1000))
+for i in A:
+    if smallest == None:
+        smallest = i
+    raznica = i - R
+    if raznica < 0:
+        raznica *= -1
+    if raznica < smallest:
+        smallest = i
+print(A)
+print("Элемент списка, который наиболее близок к числу R -", smallest)
