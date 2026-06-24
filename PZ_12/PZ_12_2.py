35b2e74e9e4ee88ce53e45f7c20cb86feb157921
GrigoriGit <grigorikurilenko@gmail.com>
2026-06-24 08:42:42 +0300

    PZ_12_2

--- PZ_12/PZ_12_2.py
+++ PZ_12/PZ_12_2.py
@@ -2,25 +2,11 @@
 
 rows = int(input("Введите количество строк матрицы: "))
 cols = int(input("Введите количество столбцов матрицы: "))
-matrix = []
-for i in range(rows):
-    row = []
-    for j in range(cols):
-        row.append(i * cols + j + 1)
-    matrix.append(row)
+matrix = [[i * cols + j + 1 for j in range(cols)] for i in range(rows)]
 print("Исходная матрица:")
-for i in range(rows):
-    for j in range(cols):
-        print(matrix[i][j], end=' ')
-    print()
-total_elements = rows * cols
-half_count = total_elements // 2
-flat_list = []
-for i in range(rows):
-    for j in range(cols):
-        flat_list.append(matrix[i][j])
-second_half_sum = 0
-for i in range(half_count, total_elements):
-    second_half_sum = second_half_sum + flat_list[i]
-
+for row in matrix:
+    print(*row)
+flat_list = [element for row in matrix for element in row]
+half_count = (rows * cols) // 2
+second_half_sum = sum(flat_list[half_count:])
 print("Сумма элементов второй половины матрицы:", second_half_sum)
